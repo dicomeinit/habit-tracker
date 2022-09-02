5761d6ff475bdf602cd6fe1fa78582aff94e7e17
Diana <dicomeinit@gmail.com>
2022-09-02 21:54:19 +0300

    Advanced Authentication using an HTTP Header

--- main.py
+++ main.py
@@ -2,12 +2,32 @@ import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
+USERNAME = "dianaber"
+TOKEN = "hihd87w9hq9cs0d0cjw093v0d9s93j"
+
 user_params = {
-    "token": "hihd87w9hq9cs0d0cjw093v0d9s93j",
-    "username": "dianaber",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Coding Graph",
+    "unit": "hour",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
