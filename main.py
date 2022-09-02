3b3eff42285b7d43d66c1ddac380f4f46aa351aa
Diana <dicomeinit@gmail.com>
2022-09-03 00:47:08 +0300

    Add a Pixel to the Habit Tracker using a Post Request

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
+import datetime
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 USERNAME = "dianaber"
 TOKEN = "hihd87w9hq9cs0d0cjw093v0d9s93j"
+GRAPH_ID = "graph1"
+
 
 user_params = {
     "token": TOKEN,
@@ -18,7 +21,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Coding Graph",
     "unit": "hour",
     "type": "float",
@@ -29,5 +32,16 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_config = {
+    "date": "20220903",
+    "quantity": "2.30",
+
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_config, headers=headers)
 print(response.text)
